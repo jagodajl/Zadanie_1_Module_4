8c0173e4730de87b1bf97ca7a1232d30baa3b57e
Jagoda Jeczmien-Lazur <jagoda.jeczmienlazur@gmail.com>
2022-07-13 23:08:26 +0200

    Poprawa vol. 2

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ logging.basicConfig(level=logging.INFO)
 
 
 def palindrome_check(word):
-    input_adjusted = word.lower().replace(" ", "")
-    return input_adjusted[::1] == input_adjusted[::-1]
+    input_without_spaces = word.lower().replace(" ", "")
+    if not input_without_spaces.isalnum():
+        logging.info("Provided input: %s contains non-alphanumeric characters. Skipping non-alphanumeric characters",
+                     word)
 
-input = ["madam", "kobyla ma maly bok", "bugi to maly szop"]
+    prepared_input = ""
+    for character in range(len(input_without_spaces)):
+        if input_without_spaces[character].isalnum():
+            prepared_input += input_without_spaces[character]
+
+    return prepared_input == prepared_input[::-1]
+
+
+input = ["madam", "kobyla ma maly bok", "bugi to maly szop", "A to kawony sama da Ada? Ma synowa kota."]
 
 for word in input:
     is_true = palindrome_check(word)
